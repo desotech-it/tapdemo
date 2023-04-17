af35e322704c223b7f039eb3e83edf83cc7f9e06
Gianluca Recchia <g.recchia@desotech.it>
2023-04-17 18:31:43 +0200

    Initial commit

---
+++ src/main/java/it/desotech/tapdemo/TapdemoApplication.java
@@ -0,0 +1,13 @@
+package it.desotech.tapdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TapdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TapdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/desotech/tapdemo/TapdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package it.desotech.tapdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TapdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
